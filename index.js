c848cf52e0f21c1a6a9377814b78d41cab04c397
Somesh banerjee <32162479+Somesh11097@users.noreply.github.com>
2022-07-07 15:24:53 +0530

    Another commit
    Learnt about variables and data types

---
+++ index.js
@@ -0,0 +1,30 @@
+// document.write("This is document write");
+
+// javascript console API
+// console.log("Hello World");
+// console.warn("this is warning");
+// console.error("This is error");
+
+//javascript variable
+
+var number1 = 34;
+var number2 = 36; 
+console.log(number1 + number2);
+
+//data stypes
+
+var str1 = "This is a string";
+var str2 = " this is another string"
+
+//objects
+var marks = {
+    ravi: 34,
+    shubham: 78,
+    somesh: 99
+}
+console.log(number2)
+console.log(str1)
+console.log(marks);
+
+var und ;
+console.log(und);
