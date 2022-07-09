2d5dc7673282bae4d52b8ba4f831329319598bee
Somesh banerjee <32162479+Somesh11097@users.noreply.github.com>
2022-07-09 19:56:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,9 @@
 
 //javascript variable
 
-var number1 = 34;
-var number2 = 36; 
-console.log(number1 + number2);
+// var number1 = 34;
+// var number2 = 36; 
+// console.log(number1 + number2);
 
 //data stypes
 
@@ -17,14 +17,30 @@ var str1 = "This is a string";
 var str2 = " this is another string"
 
 //objects
-var marks = {
-    ravi: 34,
-    shubham: 78,
-    somesh: 99
-}
-console.log(number2)
-console.log(str1)
-console.log(marks);
+// var marks = {
+//     ravi: 34,
+//     shubham: 78,
+//     somesh: 99
+// }
+// console.log(number2)
+// console.log(str1)
+// console.log(marks);
 
 var und ;
-console.log(und);
+// console.log(und);
+
+//Arrays
+//in arrays index always starts from 0 and not from 1, it is similar to all languages in programming languages
+// var arr = [1,2,3,4,5]
+// console.log(arr);
+// console.log(arr[0]);
+
+
+//operators in javascript 
+// 1.Addition
+// Subractions
+// multiplication
+// division
+var a = 55
+var b = 67
+ console.log(" Value of a + b is: ", a+b);
