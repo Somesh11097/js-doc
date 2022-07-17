c1576a10af9fdd30580e36e1cb8eb8b1c0393832
Somesh banerjee <32162479+Somesh11097@users.noreply.github.com>
2022-07-17 10:31:42 +0530

    Commit

---
+++ 1st.js
@@ -0,0 +1,21 @@
+//datatypes practice
+// console.log(10 + "20");//1020
+// console.log(9 - "5");//4
+// console.log("Java" + "Script");//javascript
+// console.log(" " + " ");//
+// console.log(" " + 0)
+// console.log("Somesh" - "Banerjee");
+
+// console.log(true + true); // 1 represents true thats y the output is 2
+
+//Q1. Difference between null vs undefined
+
+var name = null;
+console.log(name);
+console.log(typeof(name));
+
+var name1 
+console.log(name1);
+console.log(typeof(name1));
+
+
