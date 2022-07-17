542f835ca9e34d2a1cc157c6de6b2ab26eca9a4a
Somesh banerjee <32162479+Somesh11097@users.noreply.github.com>
2022-07-18 00:13:59 +0530

    Update 1st.js

--- 1st.js
+++ 1st.js
@@ -10,12 +10,46 @@
 
 //Q1. Difference between null vs undefined
 
-var name = null;
-console.log(name);
-console.log(typeof(name));
+// var name = null;
+// console.log(name);
+// console.log(typeof(name));
 
-var name1 
-console.log(name1);
-console.log(typeof(name1));
+// var name1 
+// console.log(name1);
+// console.log(typeof(name1));
+
+
+//conditionals
+//1. if else loop
+// var tom = "rain";
+// if(tom == "rain"){
+//     // console.log("Take raincoat")
+
+// }else{
+// console.log("Dry day it is")
+// }
+
+//leap year Program
+// var year = 2020;
+// debugger;
+// if(year % 4 === 0){
+//     if(year % 100 ===0){
+//         if(year % 400 ===0){
+//             console.log("leap year");
+//         }
+//         else{
+//             console.log("Not leap Year");
+//         }
+
+//     }else{
+//         console.log("Leap Year");
+//     }
+
+// }else{
+//     console.log("Not a leap year");
+// }
+
+
+
 
 
